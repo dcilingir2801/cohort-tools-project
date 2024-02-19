f7279328884213ee70236f0251e07516ca845d86
Juan I. Solis <solid.dogtraining@gmail.com>
2024-02-19 17:07:22 +0100

    days 1

--- server/app.js
+++ server/app.js
@@ -2,26 +2,28 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-
+const cors = require("cors");
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 const students = require("./students.json");
 const cohorts = require("./cohorts.json");
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
