87e5c4e370773abd6b7de58534591c84d996cd55
Deniz Cilingir <deniz.cilingir@gmx.de>
2024-02-19 15:47:51 +0100

    Day 1 Set Up

--- server/app.js
+++ server/app.js
@@ -5,7 +5,8 @@ const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+const students = require("./students.json");
+const cohorts = require("./cohorts.json");
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -29,6 +30,13 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/students", (req, res) => {
+  res.json(students);
+});
+
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
 
 // START SERVER
 app.listen(PORT, () => {
